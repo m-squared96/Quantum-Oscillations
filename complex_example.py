df79359d2dc1c54218b87252003c19113ac978ec
m-squared96 <michael.moore34@mail.dcu.ie>
2018-06-29 09:00:13 +0100

    Got scipy.integrate.ode to work, meaning complex ODE's can now be solved

--- complex_example.py
+++ complex_example.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from scipy.integrate import ode
 
 def f(t, y, arg1, arg2):
-    return -arg1*y -arg2*y
+    u, v = y
+    return [v, arg1*v -arg2*u]
 
 #Parameters
 delta = 0.0
@@ -16,19 +17,29 @@ f_arg = (1.0j * delta) - 1/tau
 jac_arg = ((abs(Omega0**2))/4) + 1.0j*(d_delta - (delta/tau))
 
 r = ode(f).set_integrator('zvode')
-r.set_initial_value(1.0,1.0).set_f_params(f_arg,jac_arg)
+r.set_initial_value([1.0, 0.0],1.0).set_f_params(f_arg,jac_arg)
 
-tl = 20
-dt = 0.001
+tl = 200
+dt = 0.01
 
 t_vals = []
 u_vals = []
+usquare_vals = []
 
 while r.successful() and r.t < tl:
     r.integrate(r.t+dt)
     t_vals.append(r.t)
     u_vals.append(r.y)
-    #print("%g %g" % (r.t, r.y))
+    usquare_vals.append((r.y)**2)
 
+plt.figure()
 plt.plot(t_vals, u_vals)
+plt.xlabel("Time")
+plt.title("u(t) and v(t)")
+
+plt.figure()
+plt.plot(t_vals, usquare_vals)
+plt.xlabel("Time")
+plt.title("abs(u(t)**2) and abs(v(t)**2)")
+
 plt.show()
