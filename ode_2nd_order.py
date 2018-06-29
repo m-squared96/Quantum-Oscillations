88262e9cf5cae6ae38b7037b0c8b5ee07a504c5f
m-squared96 <michael.moore34@mail.dcu.ie>
2018-06-29 07:13:32 +0100

    Fixed: you needed to use np.arange instead of np.linspace

--- ode_2nd_order.py
+++ ode_2nd_order.py
@@ -1,24 +1,32 @@
 from scipy.integrate import odeint
 import numpy as np
-def model(x,t):
-  y = x[0]
-  dy = x[1]  
+def model(y,t,params):
+  
+  omega_0 = params[0]
+  tau = params[1]
+
+  x = y[0]
+  dx = y[1]  
   K = (omega_0**2)/4
   xdot = [[],[]]
-  xdot[0] = dy
-  xdot[1] = (1/tau) * dy - K * y
+  xdot[0] = dx
+  xdot[1] = (1/tau) * dx - K * x
 
   return xdot
 
 omega_0 = 1/(2*np.pi)
 tau = 1000
 
-time = np.linspace(0,200,0.05)
-z = odeint(model,[2,-1],time)
+params = [omega_0, tau]
+
+t = np.arange(0,200,0.05)
+z = odeint(model,[1.0,0.0],t, args=(params,))
+print(z)
+
 
 # plot results
 import matplotlib.pyplot as plt
-plt.plot(time,z[:,0],'g:')
-plt.plot(time,z[:,1],'k-.')
+plt.plot(t,z[:,0],'g:')
+plt.plot(t,z[:,1],'k-.')
 plt.xlabel('Time')
 plt.show()
